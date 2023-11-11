9aa84572d93c72749b8d6199f3ec5be27c7af5ff
VMDang <dang.vm205063@sis.hust.edu.vn>
2023-11-12 01:36:27 +0700

    create Seeder and set status table

---
+++ database/seeders/ApartmentTypeSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ApartmentTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('apartment_types')->insert([
+            [
+                'name' => 'Apartment',
+                'description' => 'Multi-storey apartments with a large number of rooms,
+                 often serving families, high prices, long-term stays',
+            ],
+            [
+                'name' => 'Mini Apartment',
+                'description' => 'Mini apartments with a small number of floors,
+                 built for business purposes and rented in large quantities',
+            ],
+            [
+                'name' => "Motel",
+                'description' => 'Small-scale boarding houses, suitable for short-term rentals,
+                 low prices, often with few floors',
+            ],
+            [
+                'name' => 'Whole House',
+                'description' => 'Whole house but the owner does not use it but rents it out,
+                 suitable for families and small groups',
+            ],
+        ]);
+    }
+}
---
+++ database/seeders/CreateAdminAccount.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class CreateAdminAccount extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'phone' => '19008888',
+            'password' => Hash::make(env('PASSWORD_ADMIN')),
+            'status' => array_search("Active", config('app.user_status')),
+        ]);
+    }
+}
